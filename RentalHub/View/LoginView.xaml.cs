bc116da8124a2eaff00e154f56a4de82d45e4588
Ruebled <dan.canevschii@gmail.com>
2024-05-26 05:42:49 +0300

    Changed Documentation structure
    Created application logo
    
    Created login form
    
    Implemented Light/Dark Theme Support(toggle between not active yet)

---
+++ RentalHub/View/LoginView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Input;
+namespace RentalHub.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RentalHub/ViewModel/LoginViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System.Security;
+using System.Windows.Input;
+
+namespace RentalHub.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        // Fields
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        // Properties
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password 
+        { 
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage 
+        { 
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible 
+        { 
+            get => _isViewVisible;
+            set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        // -> Commands
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPassswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        // Contructor
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPassswordCommand = new ViewModelCommand(p => ExecuteRecoverPassCommand("", ""));
+        }
+
+        private void ExecuteRecoverPassCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if(string.IsNullOrEmpty(Username) || 
+               Username.Length<3 || Password == null || Password.Length < 3)
+            {
+                validData = false;
+            }
+            else
+            {
+                validData = true;
+            }
+
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
